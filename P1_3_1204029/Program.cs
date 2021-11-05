bb77854dd564463bcc8df47af7c6e2785e437b17
fadilfebriansyah <fadilfebriansyah76@gmail.com>
2021-11-05 22:09:07 +0700

    push ketiga

---
+++ P1_3_1204029/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_3_1204029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("ANAK AYAM TURUN  ");
+
+
+            int ja = Convert.ToInt16(Console.ReadLine());
+
+            int aym = ja;
+
+            for (int i = ja; i > 1; i--)
+            {
+                Console.WriteLine("Anak ayam turunlah " + ja-- + ",  mati satu tinggallah " + ja);
+            }
+            Console.WriteLine("Anak ayam turunlah " + ja + ",  mati satu tinggallah induknya ");
+
+        }
+    }
+}
